62ff98321a50ce9bd16eac1a12a6ec304917cdf9
Shouq bin daws <127682553+ShouqAbdulrahim@users.noreply.github.com>
2025-09-28 16:17:10 +0300

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,7 +1,8 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  address_space       = var.cidr_block
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
+resource "azurerm_virtual_network" "virtual_network" {
+ name                = var.vnet_name
+ address_space       = var.cidr_block
+ location            = var.location
+ resource_group_name = azurerm_resource_group.resource_group.name
+
+ tags = var.tags
 }
