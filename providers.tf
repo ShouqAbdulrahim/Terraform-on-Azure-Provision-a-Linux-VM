8e5fd4ac2806decde669101d9ca1e4d695d1d573
Shouq bin daws <127682553+ShouqAbdulrahim@users.noreply.github.com>
2025-09-28 16:07:19 +0300

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.1"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "sub//put your subscription_id "
+}
+
+provider "random" {}
+provider "tls" {}
